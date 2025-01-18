31a11071c8d5b38bf284efe9626c1a9728c2ca0f
Jakaria <jakariait@gmail.com>
2025-01-18 22:51:24 +0600

    Add files via upload

--- task_api.sql
+++ task_api.sql
@@ -1,22 +1,75 @@
--- Active: 1736774964041@@127.0.0.1@3306@task_api
-CREATE TABLE tasks(
-   id INT PRIMARY KEY AUTO_INCREMENT,
-   title VARCHAR(100) NOT NULL UNIQUE,
-   description TEXT,
-   priority ENUM('low', 'medium', 'high') DEFAULT 'low',
-   is_complete TINYINT NOT NULL DEFAULT 0 COMMENT '0: Not Completed, 1: Completed',
-   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
-INSERT INTO tasks (title, description, priority, is_complete) 
-VALUES 
-('Task 1', 'Description for Task 1', 'low', 0),
-('Task 2', 'Description for Task 2', 'medium', 1),
-('Task 3', 'Description for Task 3', 'high', 0),
-('Task 4', 'Description for Task 4', 'medium', 0),
-('Task 5', 'Description for Task 5', 'low', 1);
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jan 18, 2025 at 05:50 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
-ALTER TABLE tasks CHANGE is_complete is_completed TINYINT NOT NULL DEFAULT 0;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-SELECT * FROM tasks
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `task_api`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tasks`
+--
+
+CREATE TABLE `tasks` (
+  `id` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `description` text DEFAULT NULL,
+  `priority` enum('low','medium','high') DEFAULT 'low',
+  `is_completed` tinyint(4) NOT NULL DEFAULT 0,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tasks`
+--
+
+INSERT INTO `tasks` (`id`, `title`, `description`, `priority`, `is_completed`, `created_at`, `updated_at`) VALUES
+(2, 'Task 2', 'Description for Task 2', 'medium', 1, '2025-01-18 12:20:07', '2025-01-18 12:20:07'),
+(3, 'Task 3', 'Description for Task 3', 'high', 0, '2025-01-18 12:20:07', '2025-01-18 12:20:07'),
+(4, 'Task 4', 'Description for Task 4', 'medium', 0, '2025-01-18 12:20:07', '2025-01-18 12:20:07'),
+(5, 'Task 5', 'Description for Task 5', 'low', 1, '2025-01-18 12:20:07', '2025-01-18 12:20:07'),
+(6, 'My Fourth Task', 'This is a Fourth task.', 'medium', 0, '2025-01-18 16:22:54', '2025-01-18 16:22:54');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `title` (`title`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tasks`
+--
+ALTER TABLE `tasks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
